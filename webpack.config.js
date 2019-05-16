036cb6c30f01f8b3a34d771c1c573d832cc8df75
William Chan <wchan1992@gmail.com>
2019-05-15 20:29:13 -0700

    Update

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,24 @@
-var path = require('path');
-var SRC_DIR = path.join(__dirname, '/client/src');
-var DIST_DIR = path.join(__dirname, '/public/dist');
+const path = require('path');
+
+const SRC_DIR = path.join(__dirname, '/client/src');
+const DIST_DIR = path.join(__dirname, '/public/dist');
 
 module.exports = {
   entry: `${SRC_DIR}/index.jsx`,
   output: {
     filename: 'bundle.js',
-    path: DIST_DIR
+    path: DIST_DIR,
   },
   module: {
     loaders: [
       {
         test: /\.jsx?/,
         include: SRC_DIR,
-        loader: 'babel-loader',      
+        loader: 'babel-loader',
         query: {
-          presets: ['react', 'env']
-        }
-      }
-    ]
-  }
+          presets: ['react', 'env'],
+        },
+      },
+    ],
+  },
 };
