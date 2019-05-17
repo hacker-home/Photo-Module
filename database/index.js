f6028b09052f7a6d6692af1c7b18f3aba122424d
William Chan <wchan1992@gmail.com>
2019-05-16 19:01:22 -0700

    Add functions for seeding database with 100 mock listings

--- database/index.js
+++ database/index.js
@@ -1,12 +1,23 @@
 const mongoose = require('mongoose');
+const helper = require('./generateListings');
 
-mongoose.connect('mongodb://localhost/photos');
+mongoose.connect('mongodb://localhost/photos', { useNewUrlParser: true });
 
 const listingSchema = mongoose.Schema({
-  listingID: Number,
+  listingID: { type: Number, unique: true },
   listingDesc: String,
   isSaved: Boolean,
   listingPhotos: [{ url: String, desc: String, isVerified: Boolean }],
 });
 
 const Listing = mongoose.model('Listing', listingSchema);
+
+const listingsArray = helper.generateListings();
+
+Listing.insertMany(listingsArray, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Successfully seeded database!');
+  }
+});
