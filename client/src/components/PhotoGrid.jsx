e4a8a167fabdf477b9dabe16aa64d5bc0c96f929
William Chan <wchan1992@gmail.com>
2019-05-24 19:28:37 -0700

    Complete rendering of ShareModal componenent

--- client/src/components/PhotoGrid.jsx
+++ client/src/components/PhotoGrid.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import ShareButton from './ShareButton.jsx';
-import SaveButton from './SaveButton.jsx';
-import ViewPhotosButton from './ViewPhotosButton.jsx';
+// import ShareButton from './ShareButton.jsx';
+// import SaveButton from './SaveButton.jsx';
+// import ViewPhotosButton from './ViewPhotosButton.jsx';
 
 class PhotoGrid extends Component {
   constructor(props) {
@@ -16,33 +16,22 @@ class PhotoGrid extends Component {
     const { photos } = this.props;
 
     return (
-      <div className="photo-grid-container">
-
-        <div className="photo-grid">
-          <div className="photo-container first-photo">
-            <img className="photo" src={photos[0]} alt="" />
-          </div>
-          <div className="photo-container col-2 photo-two">
-            <img className="photo" src={photos[1]} alt="" />
-          </div>
-          <div className="photo-container col-3 photo-three">
-            <img className="photo" src={photos[2]} alt="" />
-          </div>
-          <div className="photo-container col-2 photo-four">
-            <img className="photo" src={photos[3]} alt="" />
-          </div>
-          <div className="photo-container col-3 photo-five">
-            <img className="photo" src={photos[4]} alt="" />
-          </div>
+      <div className="photo-grid">
+        <div className="photo-container first-photo">
+          <img className="photo" src={photos[0]} alt="" />
         </div>
-
-        <div className="share-and-save-buttons">
-          <ShareButton />
-          <SaveButton />
+        <div className="photo-container col-2 photo-two">
+          <img className="photo" src={photos[1]} alt="" />
         </div>
-
-        <ViewPhotosButton />
-
+        <div className="photo-container col-3 photo-three">
+          <img className="photo" src={photos[2]} alt="" />
+        </div>
+        <div className="photo-container col-2 photo-four">
+          <img className="photo" src={photos[3]} alt="" />
+        </div>
+        <div className="photo-container col-3 photo-five">
+          <img className="photo" src={photos[4]} alt="" />
+        </div>
       </div>
     );
   }
